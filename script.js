e0b68c45ffac13764de2da94eb1d3ee8eaed0201
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-10-23 22:29:13 +0530

    Update shared tweet

--- script.js
+++ script.js
@@ -45,17 +45,8 @@ function getQuote() {
 
     $('#tweet-quote').attr(
         'href',
-        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-            encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
-    );
-
-    $('#tumblr-quote').attr(
-        'href',
-        'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
-            encodeURIComponent(currentAuthor) +
-            '&content=' +
-            encodeURIComponent(currentQuote) +
-            '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+        'https://twitter.com/intent/tweet?hashtags=quote-me&related=freecodecamp&text=' +
+            encodeURIComponent('"' + currentQuote + '" - ' + currentAuthor)
     );
 
     $('.quote-text').animate({opacity: 0}, 500, function () {
